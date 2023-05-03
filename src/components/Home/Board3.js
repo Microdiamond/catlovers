4c3d81699a4b6d0a45daab230d517b2bda8c43c7
Microdiamond <chatchai.nak1997@gmail.com>
2023-05-03 11:01:22 +0700

    Board3

--- src/components/Home/Board3.js
+++ src/components/Home/Board3.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import "./Home.css";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 const Board3 = () => {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Board from '../components/Home/Board'
 import Board2 from '../components/Home/Board2'
+import Board3 from '../components/Home/Board3'
 
 const Home = () => {
   return (
     <div>
         <Board></Board>
         <Board2></Board2>
+        <Board3></Board3>
     </div>
   )
 }
